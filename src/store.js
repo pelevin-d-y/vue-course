60f33d43c3df7490ccfabeddcfcb21a8578d74c2
Denis Pelevin <natpuot1992@gmail.com>
2017-11-24 14:02:27 +0300

    commit

--- src/store.js
+++ src/store.js
@@ -6,10 +6,16 @@ Vue.use(Vuex)
 const store = new Vuex.Store ({
   state: {
     navbarTitle: 'title Vuex',
-    getters: {
-      titleCount: (state) => {
-        state.navbarTitle.length
-      }
+  },
+
+  getters: {
+    titleCount: (state) => {
+      return state.navbarTitle.length
+    }
+  },
+  mutations: {
+    changeTitle(state, payload) {
+      state.navbarTitle = payload
     }
   }
 })
